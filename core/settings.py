da3ea1ea3b7ae2cdfd15d3e1960d9d0230bfd7d0
Nishanth1205-web <nishantharasu1205@gmail.com>
2026-09-18 10:28:29 +0530

    To-Do1

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,10 +86,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(
+        "postgresql://to_do_26qr_user:N6Zm1v2jUfBN0eCJ5kzjv6UIoYvOgEFc@dpg-damc4iv40ujc73acdiog-a/to_do_26qr"
+    )
 }
 
 
